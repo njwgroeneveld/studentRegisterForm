d8e9b8bee92123958080c03a47555ff73c990373
niels <njwgroeneveld@gmail.com>
2021-11-06 16:22:00 +0100

    add error description

---
+++ functions.js
@@ -0,0 +1,29 @@
+function registerStudent(){
+    console.log("test");
+    let newNameInput = document.getElementById('nameInput').value;
+    let newEmailInput = document.getElementById('emailInput').value;
+    let newDobInput = document.getElementById('dobInput').value;
+    let newStudent = {
+        name: newNameInput,
+        email: newEmailInput,
+        dob: newDobInput
+
+    }
+
+    fetch("http://localhost:8080/api/v1/student/", {
+        method: 'POST',
+        headers: {
+            'Content-Type' : `application/json`
+        },
+        body: JSON.stringify(newStudent)
+        })
+        .then(Response => {
+            alert(`is goedgegaan`);
+        })
+        .catch(error => { 
+          alert(`Er is iets fout gegaan`);
+    //handle error --
+        });
+
+
+}
